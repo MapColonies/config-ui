2ba8c3ccf2632a1194f158a9b96046674970541f
almog8k <almog8k@gmail.com>
2024-07-15 13:21:21 +0300

    fix: merge conflict

--- src/pages/createConfig/step3ReviewAndApprove/step3ReviewAndApprove.tsx
+++ src/pages/createConfig/step3ReviewAndApprove/step3ReviewAndApprove.tsx
@@ -13,11 +13,12 @@ type ModeDetails = {
 
 export const Step3ReviewAndApprove: React.FC = () => {
   const { state } = useConfigForm();
-  const { nextVersion, mode, rollBackVersion, previousVersion, latestConfigData } = state.formData.step3;
+  const { nextVersion, mode, rollBackVersion, previousVersion } = state.formData.step3;
   const { configName } = state.formData.step1;
   const { configJsonStringData: modifiedConfig, configData } = state.formData.step2;
+  const { latestConfig } = state;
 
-  const originalConfig = useMemo(() => JSON.stringify(latestConfigData, null, 2), [latestConfigData]);
+  const originalConfig = useMemo(() => JSON.stringify(latestConfig?.config, null, 2), [latestConfig?.config]);
 
   const getModeDetails = (): ModeDetails => {
     switch (mode) {
