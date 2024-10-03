77f45c623e811716e58895de3ebf366a1fa2a898
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2024-10-03 10:40:44 +0300

    build: fixed app not working after build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,21 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': { target: 'http://localhost:8080', changeOrigin: true, rewrite: (path) => path.replace(/^\/api/, '') },
+    },
+  },
+  // hotfix based on https://github.com/JS-DevTools/ono/issues/19#issuecomment-1719659002 - not ideal but works
+  build: {
+    rollupOptions: {
+      plugins: [
+        replace({
+          delimiters: ['', ''],
+          preventAssignment: true,
+          values: {
+            'if (typeof module === "object" && typeof module.exports === "object") {':
+              'if (typeof module === "object" && typeof module.exports === "object" && typeof module.exports.default === "object") {',
+          },
+        }),
+      ],
     },
   },
 });
